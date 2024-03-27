2e1b79d2f34d4bc95a2adf4aebca6ecde82cdee2
Zouhair <user@MacBook-Pro-de-user.local>
2024-03-27 12:58:31 +0000

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ljsq0mh4',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'vue-school-showdown',
+
+  projectId: 'ljsq0mh4',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
